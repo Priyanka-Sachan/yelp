6d7eaea8ea636da7dfd2f6e30a671135e22f595e
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2021-01-11 16:55:33 +0530

    Error handling

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 
 const Campground = require('./models/campground');
+const AppError = require('./utils/appError');
+const catchAsync = require('./utils/catchAsync');
+const { campgroundSchema } = require('./JoiSchema');
 
 //Connecting to mongoose
-mongoose.connect('mongodb://localhost:27017/yelpCampDB', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/yelpCampDB', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() => {
         console.log('CONNECTED TO MONGODB');
     })
@@ -31,60 +34,81 @@ app.use(methodOverride('_method'));//making illusion of different method(get/pos
 app.use(express.urlencoded({ extended: true })); //middleware used to parse url ?q=..
 app.use(express.json());//to parse json
 
+//Middleware to validate campground data
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const message = error.details.map(err => err.message).join(',');
+        throw new AppError(400, message);
+    }
+    else
+        next();
+}
+
 app.get('/', (req, res) => {
     res.send('<h1>WELCOME TO YELPCAMP!!!</h1>');
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({});
+    if (!campgrounds)
+        throw next(new AppError(404, 'Products Not Found'));
     res.render('campground/index', { campgrounds });
-});
+}));
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
-    try {
-        await campground.save();
-        console.log(campground);
-        res.redirect(`/campgrounds/${campground._id}`);
-    } catch (e) {
-        res.status(404).send(`STATUS 404: NOT FOUND`);
-    }
-});
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
 
-app.patch('/campgrounds/:id', async (req, res) => {
+app.patch('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = req.body.campground;
     console.log(campground);
     await Campground.findByIdAndUpdate(id, campground);
     res.redirect(`/campgrounds/${id}`);
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-});
+}));
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campground/new');
 });
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    try {
-        const campground = await Campground.findById(id);
-        //console.log(...campground);
-        //console.log({...campground});
-        res.render('campground/show', { campground });
-    } catch (e) {
-        res.status(404).send(`STATUS 404: NOT FOUND`);
-    }
-});
+    // For errors returned from async functions invoked by route handlers & middlewares,
+    //  you must pass them to the next() function, where EXpress will catch n process them...
+    const campground = await Campground.findById(id);
+    if (!campground)
+        throw next(new AppError(404, 'Product Not Found'));
+    res.render('campground/show', { campground });
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
+    //instead of return, throw can also work here..
+    if (!campground)
+        throw next(new AppError(404, 'Product Not Found'));
     res.render('campground/edit', { campground });
+}));
+
+app.all('*', (req, res, next) => {
+    next(new AppError(404, 'Page Not Found'));
+});
+
+app.use((err, req, res, next) => {
+    if (!err.status)
+        err.status = 500;
+    if (!err.message)
+        err.message = 'Something went wrong!!'
+    res.status(err.status).render('error', { err });
 });
 
 //Listening to express
